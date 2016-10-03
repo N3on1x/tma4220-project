f7e8822b5bbfdedbb18aecdad21ac701676b0aad
petter <petter@dhcp-10-22-38-61.wlan.ntnu.no>
2016-10-03 15:35:11 +0200

    lagt til oppgave 1 og deler av 2

---
+++ MATLAB/problem_2/analytic_disk.m
@@ -0,0 +1,11 @@
+% Analytical solution for problem 2 on the unit disk.
+close all;
+n = 100; % Number of mesh points
+P = getDisk(n);
+x = P(:,1);
+y = P(:,2);
+tri = delaunay(x,y);
+
+z = x.*y.*sin(2*pi*(x.^2+y.^2));
+trisurf(tri,x,y,z) % Plot the function z evaluated in the mesh points
+%triplot(tri,x,y) % Plot the mesh in 2D
