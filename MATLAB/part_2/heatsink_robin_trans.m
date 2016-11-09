428514eaa6214a48ffefc33deccd82a4855c2b81
Petter Frisvåg <petter_fris@hotmail.com>
2016-11-09 11:44:06 +0100

    endret litt på heatsink_robin_trans.m

--- MATLAB/part_2/heatsink_robin_trans.m
+++ MATLAB/part_2/heatsink_robin_trans.m
@@ -34,7 +34,6 @@ for i = 1:nk
     k = tet(i,:); % Nodes of the current element
     phi_sys = [p(k,:) ones(np, 1)];
     coeff_mat = phi_sys\eye(np); % Collumn i is [a_i; b_i; c_i; d_i]
-    phi = @(x,y,z) ([x y z 1]*coeff_mat)'; % phi vector
     J_phi = coeff_mat(1:nd,:)'; % Jacobian of the vector function phi
 
     volume = quadrature3D(p(k(1),:), p(k(2),:), p(k(3),:), p(k(4),:), 1, @(x,y,z) 1); % Volume of current element.
@@ -43,21 +42,23 @@ for i = 1:nk
 end
 
 % Split the edge into the different boundries
-edge_d_ind   = find(data_edge(:,4) == 1);
+edge_d_ind = find(data_edge(:,4) == 1);
 edge_r_ind = find(data_edge(:,4) ~= 1);
-
-edge_d   = edge(edge_d_ind,:);
+edge_d = edge(edge_d_ind,:);
 edge_r = edge(edge_r_ind,:);
+
 % Enfore Robin boundary condition
 for i = 1:length(edge_r)
     k = edge_r(i,:); % Current triangle on the Robin boundary
+    
     pt = p(k,:); % Coordinates
     nv = cross(pt(2,:)-pt(1,:),pt(3,:)-pt(1,:)); % Normal vector of the plane
+    z = @(x,y) (dot(nv,pt(1,:))-nv(1)*x-nv(2)*y)/nv(3); % Equation of the plane
+    
     phi_sys = [[p(k,:); nv] ones(np, 1)];
     coeff_mat = phi_sys\eye(np); % Collumn i is [a_i; b_i; c_i; d_i]
     coeff_mat(:,np) = [];
     phi = @(x,y,z) ([x y z 1]*coeff_mat)'; % phi vector
-    z = @(x,y) (dot(nv,pt(1,:))-nv(1)*x-nv(2)*y)/nv(3); % Equation of a plane
     A_local = quadrature2D(pt(1,1:2), pt(2,1:2), pt(3,1:2), 4, @(x,y) phi(x,y,z(x,y))*(phi(x,y,z(x,y))')*norm(nv)/abs(nv(3)));
     A(k,k) = A(k,k) + A_local;
     b_local = T_amb*quadrature2D(pt(1,1:2), pt(2,1:2), pt(3,1:2), 4, @(x,y) phi(x,y,z(x,y))*norm(nv)/abs(nv(3)));
